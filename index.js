cb80abf67a19c57c11669e035103c4d5c6622437
jinxy <70546159+JinxGT@users.noreply.github.com>
2021-03-06 13:17:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,17 +7,27 @@ const prefix = "[Your Prefix]"
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag} :)`);
-    bot.user.setActivity("[your custom status]", { // THIS IS THE PLACE YOU PUT YOUR CUSTOM STATUS
-        type: "STREAMING", // THIS IS THE STATUS TYPE
-        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" // THIS IS THE LINK IT WILL BRING YOU TO WHEN YOU CLICK "Watch Stream"!
+    bot.user.setActivity("[your custom status]", { 
+        type: "STREAMING", 
+        url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" 
     });
 
 }
 )
 
-// THIS IS THE ([prefix]help) COMMAND
+//rainbow name
 
-// [SCRIPT GOES HERE]
+bot.on('ready', () => {
+var colors = ['#8585ff','#fff681','#a073fd','#fd73b9'];
+    var random = Math.floor(Math.random() * colors.length);
+    var role = message.guild.roles.find("name", "role name");
+    setInterval(() => {
+        role.edit({
+            color: colors[random]
+        })
+    }, 5000);
+
+});
 
 // THIS IS THE bot.login
 
